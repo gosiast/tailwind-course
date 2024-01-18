f90bc5ea64af8a2bafa38f85fa2a52c27932658d
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-01-18 08:22:01 +0100

    added wavey animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				wavey: {
+					"0%, 100%": {
+						transform: "scaleY(0.5)",
+					},
+					"50%": {
+						transform: "scaleY(1.5)",
+					},
+				},
+			},
+			//animation-wavey, we want to extend the class
+			//infinite means it will keep working all the time
+			animation: {
+				wavey: "wavey 1s linear infinite",
+			},
+		},
 	},
 	plugins: [require("./plugins/openVariant")],
 };
